e2ea4c4fd8346aba4eec474147a1b6c4f8e3a5b0
Joew91 <26jwagner@derryfield.org>
2025-01-07 20:26:49 -0500

    at 8

---
+++ at 08/RandomApp.java
@@ -0,0 +1,6 @@
+public class RandomApp {
+    public static void main(String[] args) {
+        int number = Random.getRandomInteger(7);
+        System.out.println("The random integer is" + number);
+    }
+}
