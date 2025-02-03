4a9e49b41cae9ca8a0fd9443c890e9e2433c9d58
Joew91 <26jwagner@derryfield.org>
2025-02-03 10:31:25 -0500

    26

---
+++ lesson 26/PartA.java
@@ -0,0 +1,13 @@
+public class PartA {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("circumfrance of the circle: " + circumference);
+
+        double nummber = 16.0;
+        double squareRoot = Math.sqrt(nummber);
+        System.out.println("square rot of " + nummber + "is: " + squareRoot);
+
+    }
+
+}
