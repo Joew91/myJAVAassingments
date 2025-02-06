f7d1fb4596e1e99434489f96704c3e532f66408e
Joew91 <26jwagner@derryfield.org>
2025-02-06 17:00:41 -0500

    Update Yard.java

--- lawn mower/Yard.java
+++ lawn mower/Yard.java
@@ -29,6 +29,52 @@ public class Yard {
         new Yard(height, width).printYard();
         scanner.close();
 
-    }
+        class Mower {
+        int row, col, direction = 1;
+        
+        public Mower(int startRow, int startCol) {
+            row = startRow;
+            col = startCol;
 
-}
+        }
+        public void move() {
+            col++;
+            
+        }
+        public char getSymbol() {
+            return '>';
+            
+        public static void printYard(Mower mower) {
+            clearScreen();
+            yard[mower.row][mower.col] = mower.getSymbol();
+            for (char[] row : yard) System.out.println(row);
+            yard[mower.row] [mower.col] = ' ';
+
+         }
+         public static void clearScreen() {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+         }
+         public static void delay(long ms) {
+            try { Thread.sleep(ms); } catch (InterruptedException e) {}
+
+         }
+         public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("enter lawn height: ");
+            int height = scanner.nextInt();
+            System.out.print("enter lawn width: ");
+            int width = scanner.nextInt();
+
+            Yard yard = new Yard(height, width);
+            Mower mower = new Mower(2, 1);
+
+            while (mower.col < width) {
+                yard.printYard(mower);
+                delay(500);
+                mower.move();
+            }
+          
+         }
+         
+        }
